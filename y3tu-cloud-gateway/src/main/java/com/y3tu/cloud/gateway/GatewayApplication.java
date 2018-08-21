fd620fb1c9ed0079997d76c7cda5327cc02bbd8d
y3tu <14643092@qq.com>
2018-08-21 17:24:25 +0800

    删除一些不不要的文件

--- y3tu-cloud-gateway/src/main/java/com/y3tu/cloud/gateway/GatewayApplication.java
+++ y3tu-cloud-gateway/src/main/java/com/y3tu/cloud/gateway/GatewayApplication.java
@@ -2,7 +2,17 @@ package com.y3tu.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+/**
+ * @author y3tu
+ * 网关服务
+ */
+@EnableZuulProxy
+@EnableFeignClients
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class GatewayApplication {
 
