bce0a93048736802a1221af71075590933a8ee51
y3tu <14643092@qq.com>
2018-08-25 11:45:17 +0800

    更新

--- y3tu-cloud-auth/y3tu-cloud-auth-authentication/src/main/java/com/y3tu/cloud/auth/authentication/AuthenticationApplication.java
+++ y3tu-cloud-auth/y3tu-cloud-auth-authentication/src/main/java/com/y3tu/cloud/auth/authentication/AuthenticationApplication.java
@@ -2,13 +2,15 @@ package com.y3tu.cloud.auth.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author y3tu
  * 资源服务器
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @EnableDiscoveryClient
 public class AuthenticationApplication {
 
