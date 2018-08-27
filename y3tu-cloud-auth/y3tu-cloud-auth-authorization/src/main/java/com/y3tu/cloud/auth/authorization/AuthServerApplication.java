d950894251517fadc6763d1c4e7a110438de5b55
y3tu <14643092@qq.com>
2018-08-27 23:33:15 +0800

    更新

--- y3tu-cloud-auth/y3tu-cloud-auth-authorization/src/main/java/com/y3tu/cloud/auth/authorization/AuthServerApplication.java
+++ y3tu-cloud-auth/y3tu-cloud-auth-authorization/src/main/java/com/y3tu/cloud/auth/authorization/AuthServerApplication.java
@@ -1,21 +1,18 @@
 package com.y3tu.cloud.auth.authorization;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 @EnableHystrixDashboard
-@EnableHystrix
 @ComponentScan(basePackages = {"com.y3tu.cloud.common","com.y3tu.cloud.auth.authorization"})
 public class AuthServerApplication {
 
@@ -23,13 +20,4 @@ public class AuthServerApplication {
         SpringApplication.run(AuthServerApplication.class, args);
     }
 
-    @Bean
-    public ServletRegistrationBean getServlet() {
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
-    }
 }
