0c2850a241995628cc02920c516f87102c419c9c
y3tu <14643092@qq.com>
2018-10-05 00:23:36 +0800

    代码格式化

--- doc/oauth2.sql
+++ doc/oauth2.sql
@@ -1,79 +1,22 @@
-##access_token存储表
-DROP TABLE IF EXISTS oauth_access_token;
-CREATE TABLE oauth_access_token
-(
-  token_id          CHARACTER VARYING(256) COMMENT 'MD5加密的access_token的值',
-  token             blob COMMENT 'OAuth2AccessToken.java对象序列化后的二进制数据',
-  authentication_id CHARACTER VARYING(256) COMMENT 'MD5加密过的username,client_id,scope',
-  user_name         CHARACTER VARYING(256) COMMENT '登录的用户名',
-  client_id         CHARACTER VARYING(256) COMMENT '客户端ID',
-  authentication    blob COMMENT 'OAuth2Authentication.java对象序列化后的二进制数据',
-  refresh_token     CHARACTER VARYING(256) COMMENT 'MD5加密后的refresh_token的值'
-)
-  COMMENT '访问令牌表';
+DROP TABLE IF EXISTS `sys_oauth_client_details`;
+CREATE TABLE `sys_oauth_client_details` (
+  `client_id` varchar(40) NOT NULL,
+  `resource_ids` varchar(256) DEFAULT NULL,
+  `client_secret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `authorized_grant_types` varchar(256) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additional_information` varchar(4096) DEFAULT NULL,
+  `autoapprove` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-##refresh_token存储表
-DROP TABLE IF EXISTS oauth_refresh_token;
-CREATE TABLE oauth_refresh_token
-(
-  token_id       CHARACTER VARYING(256) COMMENT 'MD5加密过的refresh_token的值',
-  token          blob COMMENT 'OAuth2RefreshToken.java对象序列化后的二进制数据',
-  authentication blob COMMENT 'OAuth2Authentication.java对象序列化后的二进制数据'
-)
-  COMMENT '更新令牌表';
-
-##授权记录表
-DROP TABLE IF EXISTS oauth_approvals;
-CREATE TABLE oauth_approvals
-(
-  userid         CHARACTER VARYING(256) COMMENT '登录的用户名',
-  clientid       CHARACTER VARYING(256) COMMENT '客户端ID',
-  scope          CHARACTER VARYING(256) COMMENT '申请的权限',
-  status         CHARACTER VARYING(10) COMMENT '状态（Approve或Deny',
-  expiresat      DATE COMMENT '过期时间',
-  lastmodifiedat DATE COMMENT '最终修改时间'
-)
-  COMMENT '授权记录表';
-
-##授权码表
-DROP TABLE IF EXISTS oauth_code;
-CREATE TABLE oauth_code
-(
-  code           CHARACTER VARYING(256) COMMENT '授权码(未加密)',
-  authentication blob COMMENT 'AuthorizationRequestHolder.java对象序列化后的二进制数据'
-)
-  COMMENT '授权码表';
-
-##client用户表
-DROP TABLE IF EXISTS oauth_client_details;
-CREATE TABLE oauth_client_details
-(
-  client_id               CHARACTER VARYING(256) NOT NULL PRIMARY KEY
-  COMMENT '客户端ID',
-  resource_ids            CHARACTER VARYING(256) COMMENT '资源ID集合,多个资源时用逗号(,)分隔',
-  client_secret           CHARACTER VARYING(256) COMMENT '客户端密匙',
-  scope                   CHARACTER VARYING(256) COMMENT '客户端申请的权限范围',
-  authorized_grant_types  CHARACTER VARYING(256) COMMENT '客户端支持的grant_type',
-  web_server_redirect_uri CHARACTER VARYING(256) COMMENT '重定向URI',
-  authorities             CHARACTER VARYING(256) COMMENT '客户端所拥有的Spring Security的权限值，多个用逗号(,)分隔',
-  access_token_validity   INTEGER COMMENT '访问令牌有效时间值(单位:秒)',
-  refresh_token_validity  INTEGER COMMENT '更新令牌有效时间值(单位:秒)',
-  additional_information  CHARACTER VARYING(4096) COMMENT '预留字段',
-  autoapprove             CHARACTER VARYING(256) COMMENT '用户是否自动Approval操作'
-)
-  COMMENT '客户端信息';
-
-##客户端授权令牌表
-DROP TABLE IF EXISTS oauth_client_token;
-CREATE TABLE oauth_client_token
-(
-  token_id          CHARACTER VARYING(256) COMMENT 'MD5加密的access_token值',
-  token             blob COMMENT 'OAuth2AccessToken.java对象序列化后的二进制数据',
-  authentication_id CHARACTER VARYING(256) COMMENT 'MD5加密过的username,client_id,scope',
-  user_name         CHARACTER VARYING(256) COMMENT '登录的用户名',
-  client_id         CHARACTER VARYING(256) COMMENT '客户端ID'
-)
-  COMMENT '客户端授权令牌表';
-
-
-
+-- ----------------------------
+-- Records of sys_oauth_client_details
+-- ----------------------------
+INSERT INTO `sys_oauth_client_details` VALUES ('sso-demo1', null, '$2a$10$pgzIKH1c2/7jS2eYk0bK9OnShYXvWiZXd3firLJkaF2q9Ct.WVtSe', 'read,write', 'password,refresh_token,authorization_code', '', null, null, null, null, 'true');
+INSERT INTO `sys_oauth_client_details` VALUES ('sso-demo2', '', '$2a$10$0fl9mlBjfg0E48Thhe73Sekw0qdR9OTvXLl3nn8isgwc1LnDyuSD2', 'read,write', 'password,refresh_token,authorization_code', '', '', null, null, null, 'true');
+INSERT INTO `sys_oauth_client_details` VALUES ('taroco', null, '$2a$10$drYsSntNKIr.cCiAQip0uOp5VtZl2FWZ4WvNYgLcMb19ri66mVzRS', 'server', 'password,refresh_token,authorization_code', null, null, null, null, null, 'false');
