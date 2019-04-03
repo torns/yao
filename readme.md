80198a02a173895736e56f4b7e56a0c40c48f48c
y3tu <14643092@qq.com>
2019-04-03 10:12:26 +0800

    update

---
+++ readme.md
@@ -0,0 +1,21 @@
+## 后台主要技术栈
+
+- 核心框架：Spring Boot + Spring Cloud Alibaba
+- ORM 框架：tk.mybatis 简化 MyBatis 开发
+- 数据库连接池：Alibaba Druid
+- 数据库缓存：Redis Sentinel
+- 消息中间件：RocketMQ
+- 接口文档引擎：Swagger2 RESTful 风格 API 文档生成
+- 全文检索引擎：ElasticSearch
+- 分布式链路追踪：SkyWalking
+- 分布式文件系统：Alibaba OSS
+- 分布式系统网关：Spring Cloud Gateway
+- 分布式协调系统：Spring Cloud Alibaba Nacos Server
+- 分布式配置中心：Spring Cloud Alibaba Nacos Config
+- 分布式熔断降级：Spring Cloud Alibaba Sentinel
+- 反向代理负载均衡：Nginx
+
+## 前后分离
+
+- 前端框架：NodeJS + Vue + Axios
+- 前端模板：ElementUI
--- y3tu-cloud-gateway/src/main/java/com/y3tu/cloud/gateway/GatewayApplication.java
+++ y3tu-cloud-gateway/src/main/java/com/y3tu/cloud/gateway/GatewayApplication.java
@@ -1,11 +1,9 @@
 package com.y3tu.cloud.gateway;
 
 import com.y3tu.tool.web.annotation.EnableDefaultExceptionAdvice;
-import com.y3tu.tool.web.annotation.EnableRedisTool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,12 +11,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  *
  * @author y3tu
  */
-@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 @EnableDefaultExceptionAdvice
-@EnableRedisTool
 public class GatewayApplication {
 
     public static void main(String[] args) {
